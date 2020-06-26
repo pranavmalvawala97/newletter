4ad6123d2d2c8282264e7dc353dac4a066586346
discussion4all <discussion4all@yahoo.com>
2020-06-26 14:25:22 +0530

    change in backend url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const PROD_URL = "http://ec2-34-224-95-146.compute-1.amazonaws.com:9000";
+const PROD_URL = "https://ec2-34-224-95-146.compute-1.amazonaws.com:9000";
 //const DEV_URL = "http://localhost:9000";
 
 export const BASE_URL = PROD_URL;
